8ec290a5215bab45bcc4f042fcaa1864a3ea8419
Samuel Lord <sllord@icloud.com>
2018-01-05 16:12:02 -0600

    Update Admin kolsh.lua

--- Admin kolsh.lua
+++ Admin kolsh.lua
@@ -1,5 +1,5 @@
-local owners = {"NikepRro2004"} -- Are able to set admins who can ban/etc... using :pa name
-local admins = {"NikePro2004"} -- Sets admins who can use ban/kick/admin or shutdown
+local owners = {"Sam_Lord"} -- Are able to set admins who can ban/etc... using :pa name
+local admins = {"ToxicFoox"} -- Sets admins who can use ban/kick/admin or shutdown
 local tempadmins = {} -- Sets admins who can't use ban/kick/admin or shutdown
 local banland = {"isik"} -- Permanently Bans people
 local prefix = ":" -- If you wanna change how your commands start ':'kill noob
